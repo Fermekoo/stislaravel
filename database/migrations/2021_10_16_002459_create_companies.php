c1501dcbd7bbadf81f500f5f4f816d45baf0b64a
Fermekoo <dandifermeko@gmail.com>
2021-10-16 07:48:02 +0700

    init

---
+++ database/migrations/2021_10_16_002459_create_companies.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_code')->unique()->comment('ex: PR-00001');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone',20);
+            $table->string('logo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ resources/views/partials/_sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="active"><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fa fa-coins"></i> <span>Master Data</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="bootstrap-alert.html">Divisi</a></li>
+                    <li><a class="nav-link" href="bootstrap-alert.html">Perusahaan</a></li>
+                    <li><a class="nav-link" href="bootstrap-alert.html">Jabatan</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
